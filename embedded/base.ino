deb4a9e9e9647f15b17b58fdbb237d3a07a3392a
colinRawlings <colin.d.rawlings@gmail.com>
2021-06-05 18:45:55 +0200

    WIP: Try out the ArduinoJson library

--- embedded/base.ino
+++ embedded/base.ino
@@ -1,9 +1,58 @@
+#include <ArduinoJson.h>
+
 #include "Arduino.h"
 
 #include "UI.h"
 
 CUI ui(Serial);
 
-void setup() { InitSerialPort(); }
+void setup() {
+  InitSerialPort();
+
+  //
+
+  StaticJsonDocument<200> doc;
+
+  // StaticJsonDocument<N> allocates memory on the stack, it can be
+  // replaced by DynamicJsonDocument which allocates in the heap.
+  //
+  // DynamicJsonDocument doc(200);
+
+  // JSON input string.
+  //
+  // Using a char[], as shown here, enables the "zero-copy" mode. This mode uses
+  // the minimal amount of memory because the JsonDocument stores pointers to
+  // the input buffer.
+  // If you use another type of input, ArduinoJson must copy the strings from
+  // the input to the JsonDocument, so you need to increase the capacity of the
+  // JsonDocument.
+  char json[] =
+      "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
+
+  // Deserialize the JSON document
+  DeserializationError error = deserializeJson(doc, json);
+
+  // Test if parsing succeeds.
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+
+  // Fetch values.
+  //
+  // Most of the time, you can rely on the implicit casts.
+  // In other case, you can do doc["time"].as<long>();
+  const char* sensor = doc["sensor"];
+  long time = doc["time"];
+  double latitude = doc["data"][0];
+  double longitude = doc["data"][1];
+
+  // Print values.
+  Serial.println(sensor);
+  Serial.println(time);
+  Serial.println(latitude, 6);
+  Serial.println(longitude, 6);
+}
 
 void loop() { ui.Update(); }
