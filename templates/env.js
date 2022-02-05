e3e715dd73af49ffda9bd131249a57ac16477ada
ColinRawlings <colin.d.rawlings@gmail.com>
2022-02-05 18:03:28 +0100

    Fix the template

--- templates/env.js
+++ templates/env.js
@@ -2,7 +2,6 @@
     window.__env = window.__env || {};
 
     // API url
-//    window.__env.apiUrl = '{{ current_ip }}:5000';
-    window.__env.apiUrl = 'localhost:5000';
+   window.__env.apiUrl = '{{ current_ip }}:5000';
 
   }(this));
